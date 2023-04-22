72449290f0c74b7603a6f001f078d62d3d983b96
Benjamin Buettner <b.buettner@tue.nl>
2023-04-22 14:53:20 +0200

    Dark mode

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,11 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('theme-toggle');
-    const themeElement = document.documentElement;
-  
-    function toggleDarkMode() {
-      themeElement.classList.toggle('dark-mode');
+(function() {
+  const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+  if (darkModeToggle) {
+    // Check if localStorage has a value for 'dark-mode'
+    if (localStorage.getItem('dark-mode') === 'true') {
+      document.documentElement.classList.add('dark');
+      darkModeToggle.checked = true;
+    } else {
+      document.documentElement.classList.remove('dark');
+      darkModeToggle.checked = false;
     }
-  
-    toggleButton.addEventListener('click', toggleDarkMode);
-  });
-  
+
+    darkModeToggle.addEventListener('change', function() {
+      if (darkModeToggle.checked) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('dark-mode', 'true');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('dark-mode', 'false');
+      }
+    });
+  }
+})();
